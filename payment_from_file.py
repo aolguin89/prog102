bbdd9fccacb6d6c5a9e96d78488b3977d0fd6b7c
aolguin89 <alvaroarmendariz11@gmail.com>
2023-01-18 18:52:21 -0300

    Update payment_from_file.py

--- payment_from_file.py
+++ payment_from_file.py
@@ -1,19 +1,17 @@
-# asking for data
-# hs = int(input("Enter the hours worked "))
-# rate = int(input("Enter employee rate "))
-
 # Init by default payment
 payment = None
 
 # Opening the workers file
 with open('employees.csv') as file:
-    for line in file:
-        employee = line.split(",")
-        print(employee)
+    for line in file.readlines()[1:]:
+        data = line.split(",")
+        if data != ['\n']:
+            name = data[0]
+            hs = float(data[1])
+            rate = float(data[2])
+            if hs < 40:
+                payment = hs * rate
+            else:
+                payment = (hs * rate + (hs - 40) * rate * 0.5)
 
-# if hs < 40:
-#    payment = hs * rate
-# else:
-#    payment = (hs * rate + (hs - 40) * rate * 0.5)
-
-# print("The corresponding payment is: ", payment)
+            print("The corresponding paymente for: {} is: {}".format(name, payment))
